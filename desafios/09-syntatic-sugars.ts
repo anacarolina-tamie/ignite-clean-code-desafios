9a2050600be07d01a890e8db766ed917101539c8
anacarolina-tamie <yoshimura.anactamie@gmail.com>
2026-06-24 19:46:31 -0300

    refactor(ex09): rewrite the code using clean code concepts

--- desafios/09-syntatic-sugars.ts
+++ desafios/09-syntatic-sugars.ts
@@ -1,26 +1,29 @@
 function getFirstFiveRatings(ratings) {
-  return ratings.length >= 5 && ratings.slice(0, 5)
+  if (ratings.length < 5) {
+    return false;
+  }
+
+  return ratings.slice(0, 5);
 }
 
 function sumFirstFiveRatings(ratings) {
-  const ratingsBool = !!ratings
-
-  if (ratingsBool) {
-    const firstFiveRatings = getFirstFiveRatings(ratings)
-
-    if (!firstFiveRatings) return { error: 'there must be at least 5 ratings' }
-
-    let ratingsSum = 0;
-
-    for (const rating of firstFiveRatings) {
-      ratingsSum += +rating
-    }
-
-    return { ratingsSum, created_at: +new Date() }
+  if (!ratings) {
+    return { error: 'ratings is required' };
   }
 
-  return { error: 'ratings is required' }
+  const firstFiveRatings = getFirstFiveRatings(ratings)
+
+  if (!firstFiveRatings) return { error: 'there must be at least 5 ratings' }
+
+  let ratingsSum = 0;
+
+  for (const rating of firstFiveRatings) {
+    ratingsSum += Number(rating)
+  }
+
+  return { ratingsSum, created_at: Date.now() }
 }
 
 const appRatings = ['5', '3', '4', '4', '5', '1', '5', '4', '4', '3']
-sumFirstFiveRatings(appRatings)
+
+console.log(sumFirstFiveRatings(appRatings));
