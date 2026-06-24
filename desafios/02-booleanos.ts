e7252ac88a2813aa67aa8dd405e56b71377c9a46
anacarolina-tamie <yoshimura.anactamie@gmail.com>
2026-06-24 15:47:45 -0300

    refactor(ex02): rename variables using clean code concepts

--- desafios/02-booleanos.ts
+++ desafios/02-booleanos.ts
@@ -2,7 +2,7 @@
 
 const user = {
   name: 'Diego Fernandes',
-  height: 190,
+  height: 120,
   hasTicket: true,
 }
 
@@ -10,21 +10,21 @@ const necessaryHeight = 130
 
 const currentHour = new Date().getHours()
 
-const parkOpen = currentHour > 9 && currentHour < 18
+const isParkOpen = currentHour > 9 && currentHour < 18
 
-if (!parkOpen) {
+if (!isParkOpen) {
   throw new Error('O parque está fechado!')
 }
 
-const ticket = user.hasTicket
+const hasTicket = user.hasTicket
 
-if (!ticket) {
+if (!hasTicket) {
   throw new Error('O Diego não possui um bilhete para entrar no parque!')
 }
 
-const enterToy = user.height > necessaryHeight
+const canEnterToy = user.height > necessaryHeight
 
-if (!enterToy) {
+if (!canEnterToy) {
   throw new Error('O Diego não pode entrar no brinquedo!')
 } 
 
